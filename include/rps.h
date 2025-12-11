6e1922862bc4ab9f85f0386e65d91ad204e0963a
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-11 19:13:50 +0100

    feat: emojis

---
+++ include/rps.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "common.h"
+
+#define rocks_count     (3)
+#define papers_count    (3)
+#define scissors_count  (2)
+
+copied const char * const rocks[rocks_count] = {
+    "✊",
+    "👊",
+    "🪨",
+};
+
+copied const char * papers[papers_count] = {
+    "✋",
+    "🫱",
+    "📜",
+};
+
+copied const char * scissors[scissors_count] = {
+    "✌️",
+    "✂️",
+};
+
+copied const char * trophy    = "🏆";
+copied const char * attention = "👀";
+copied const char * defeated  = "😵";
+
