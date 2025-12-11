957c79d698f4c0dfaff25f17696db274a12fd0b0
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-11 20:30:39 +0100

    feat: keyboard event detection

--- src/keys.c
+++ src/keys.c
@@ -14,10 +14,192 @@ static bool _timeout_enabled = false;
 
 static copied key_t keyboard_key_event_esc();
 static copied key_t keyboard_key_event_ctrl(copied const key_t key);
+static copied key_t keyboard_key_event_ctrl_alt(copied const key_t key);
+static copied key_t keyboard_key_event_csi();
+static copied key_t keyboard_key_event_csi_ext(copied const key_t key);
+static copied key_t keyboard_key_event_ss3();
+
+static copied key_t keyboard_key_event_csi_ext(copied const key_t key)
+{
+    copied int32_t n = key - '0';
+
+    copied int32_t b = terminal_raw_byte_read();
+    if (b == -1)
+    {
+        keyboard_event_timeout_disable();
+        return key_unknown;
+    }
+
+    if ('0' <= b && b <= '9')
+    {
+        n = (n * 10) + (b - '0');
+        b = terminal_raw_byte_read();
+        keyboard_event_timeout_disable();
+        if (b != '~')
+        {
+            return key_unknown;
+        }
+    }
+
+    switch (n)
+    {
+        case 1:     return key_home;
+        case 2:     return key_insert;
+        case 3:     return key_delete;
+        case 4:     return key_end;
+        case 5:     return key_page_up;
+        case 6:     return key_page_down;
+
+        case 11:    return key_f1;
+        case 12:    return key_f2;
+        case 13:    return key_f3;
+        case 14:    return key_f4;
+        case 15:    return key_f5;
+        case 17:    return key_f6;
+        case 18:    return key_f7;
+        case 19:    return key_f8;
+        case 20:    return key_f9;
+        case 21:    return key_f10;
+        case 23:    return key_f11;
+        case 24:    return key_f12;
+
+        default:    return key_unknown;
+    }
+}
+
+static copied key_t keyboard_key_event_csi()
+{
+    copied int32_t b = terminal_raw_byte_read();
+    switch (b)
+    {
+        case 'A':
+        {
+            keyboard_event_timeout_disable();
+            return key_up;
+        } break;
+
+        case 'B':
+        {
+            keyboard_event_timeout_disable();
+            return key_down;
+        } break;
+
+        case 'C':
+        {
+            keyboard_event_timeout_disable();
+            return key_right;
+        } break;
+
+        case 'D':
+        {
+            keyboard_event_timeout_disable();
+            return key_left;
+        } break;
+
+        case 'H':
+        {
+            keyboard_event_timeout_disable();
+            return key_home;
+                ;
+        } break;
+
+        case 'F':
+        {
+            keyboard_event_timeout_disable();
+            return key_end;
+        } break;
+
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        {
+            return keyboard_key_event_csi_ext(b);
+        } break;
+
+        default:
+        {
+            keyboard_event_timeout_disable();
+            return key_none;
+        } break;
+    }
+}
+
+static copied key_t keyboard_key_event_ss3()
+{
+    copied int32_t key = terminal_raw_byte_read();
+    keyboard_event_timeout_disable();
+    switch (key)
+    {
+        case 'P': return key_f1     ;
+        case 'Q': return key_f2     ;
+        case 'R': return key_f3     ;
+        case 'S': return key_f4     ;
+        case 'H': return key_home   ;
+        case 'F': return key_end    ;
+        /* Some terminals also send these for arrows */
+        case 'A': return key_up     ;
+        case 'B': return key_down   ;
+        case 'C': return key_right  ;
+        case 'D': return key_left   ;
+        default : return key_unknown;
+    }
+}
 
 static copied key_t keyboard_key_event_esc()
 {
     keyboard_event_timeout_enable();
+
+    copied int32_t b = terminal_raw_byte_read();
+    if (b == -1)
+    {
+        keyboard_event_timeout_disable();
+        return key_esc;
+    }
+
+    // alt + <key>
+    if (0x20 <= b && b < 0x7f && b != '[' && b != 'O')
+    {
+        keyboard_event_timeout_disable();
+        return (alt_mask | b);
+    }
+
+    // ctrl + alt + <key>
+    if (0x01 <= b && b <= 0x1a)
+    {
+        return keyboard_key_event_ctrl_alt(b);
+    }
+
+    // CSI
+    if (b == '[')
+    {
+        return keyboard_key_event_csi();
+    }
+
+    // SS3
+    if (b == 'O')
+    {
+        return keyboard_key_event_ss3();
+    }
+
+    keyboard_event_timeout_disable();
+    return key_unknown;
+}
+
+static copied key_t keyboard_key_event_ctrl_alt(copied const key_t key)
+{
+    keyboard_event_timeout_disable();
+    if (key == key_tab || key == key_enter)
+    {
+        return key;
+    }
+    return (ctrl_mask | alt_mask | (key - 1 + 'a'));
 }
 
 static copied key_t keyboard_key_event_ctrl(copied const key_t key)
