84985bb9908b2b6421fa4e60db17fa7c37e6571e
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-11 19:47:49 +0100

    feat: keys api

---
+++ include/keys.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "common.h"
+
+#define ctrl_mask           (0x1000)
+#define alt_mask            (0x2000)
+#define shift_mask          (0x4000)
+#define mask                (ctrl_mask | alt_mask | shift_mask)
+#define baseof(key)         ((key) & (~mask))
+
+typedef int32_t key_t;
+
+enum
+{
+    key_unknown   = -2,
+    key_none      = -1,
+
+    key_tab       = 9,      /* ctrl + i */
+    key_enter     = 13,     /* ctrl + m */
+    key_backspace = 127,
+    key_esc       = 256,
+
+    key_up,
+    key_down,
+    key_left,
+    key_right,
+    key_home,
+    key_end,
+    key_page_up,
+    key_page_down,
+
+    key_insert,
+    key_delete,
+
+    key_f1,
+    key_f2,
+    key_f3,
+    key_f4,
+    key_f5,
+    key_f6,
+    key_f7,
+    key_f8,
+    key_f9,
+    key_f10,
+    key_f11,
+    key_f12,
+};
+
+copied key_t terminal_key_read();
+
+borrowed const char * key_name_map(copied const key_t key);
+
